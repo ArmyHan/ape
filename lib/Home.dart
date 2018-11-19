8c9a5b6ca0efa0df3182c9b093a6db904bea8522
wujianchuan <wujianchuan@shinow.com>
2018-11-19 11:47:46 +0800

    添加弹出菜单。

--- lib/Home.dart
+++ lib/Home.dart
@@ -26,46 +26,7 @@ class HomeState extends State<Home> with SingleTickerProviderStateMixin {
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: _buildAppBar(),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text(
-                '阿猿',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-              ),
-              accountEmail: Text('ayuan@dingding.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    'https://avatars1.githubusercontent.com/u/35946000?s=400&u=56381a9606a5263b20b991db35fe6747bf9959a7&v=4'),
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.yellow[400],
-                  image: DecorationImage(
-                      image: AssetImage(headBackgroundImage),
-                      fit: BoxFit.fill,
-                      colorFilter: ColorFilter.mode(Colors.grey[100].withOpacity(0.5), BlendMode.hardLight))),
-            ),
-            ListTile(
-                title: Text('Messages', textAlign: TextAlign.right),
-                trailing: Icon(Icons.message, size: 22),
-                onTap: () => Navigator.pop(context)),
-            ListTile(
-                title: Text('Favorite', textAlign: TextAlign.right),
-                trailing: Icon(Icons.favorite, size: 22),
-                onTap: () => Navigator.pop(context)),
-            ListTile(
-                title: Text('Settings', textAlign: TextAlign.right),
-                trailing: Icon(Icons.settings, size: 22),
-                onTap: () => Navigator.pop(context)),
-            ListTile(
-                title: Text('Logout', textAlign: TextAlign.right),
-                trailing: Icon(Icons.exit_to_app, size: 22),
-                onTap: () => Navigator.pop(context)),
-          ],
-        ),
-      ),
+      drawer: _buildDrawer(),
       body: TabBarView(
         controller: _controller,
         children: <Widget>[
@@ -80,8 +41,7 @@ class HomeState extends State<Home> with SingleTickerProviderStateMixin {
           controller: _controller,
           unselectedLabelColor: Colors.black38,
           indicatorColor: Colors.black54,
-          indicatorSize: TabBarIndicatorSize.label,
-          indicatorWeight: 1.0,
+          indicatorWeight: 0.5,
           tabs: <Widget>[
             Tab(icon: Icon(Icons.home)),
             Tab(icon: Icon(Icons.notifications)),
@@ -98,17 +58,71 @@ class HomeState extends State<Home> with SingleTickerProviderStateMixin {
       centerTitle: false,
       elevation: 0.0,
       actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.search),
-          tooltip: 'Search',
-          onPressed: () => debugPrint('search button press.'),
-        ),
-        IconButton(
+        PopupMenuButton(
           icon: Icon(Icons.more_horiz),
           tooltip: 'More',
-          onPressed: () => debugPrint('more button press.'),
+          onSelected: (String value) => debugPrint(value),
+          itemBuilder: (context) => <PopupMenuItem<String>>[
+                PopupMenuItem(
+                  value: "Search",
+                  child: ListTile(
+                      title: Text('Search Button', textAlign: TextAlign.right),
+                      trailing: Icon(Icons.search, size: 22),
+                      onTap: () => Navigator.pop(context)),
+                ),
+                PopupMenuItem(
+                  value: "Settings",
+                  child: ListTile(
+                      title: Text('Setting Button', textAlign: TextAlign.right),
+                      trailing: Icon(Icons.settings, size: 22),
+                      onTap: () => Navigator.pop(context)),
+                ),
+              ],
         ),
       ],
+    );
+  }
+
+  _buildDrawer() {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text(
+              '阿猿',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+            ),
+            accountEmail: Text('ayuan@dingding.com'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://avatars1.githubusercontent.com/u/35946000?s=400&u=56381a9606a5263b20b991db35fe6747bf9959a7&v=4'),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.yellow[400],
+                image: DecorationImage(
+                    image: AssetImage(headBackgroundImage),
+                    fit: BoxFit.fill,
+                    colorFilter: ColorFilter.mode(Colors.grey[400].withOpacity(0.4), BlendMode.hardLight))),
+          ),
+          ListTile(
+              title: Text('Messages', textAlign: TextAlign.right),
+              trailing: Icon(Icons.message, size: 22),
+              onTap: () => Navigator.pop(context)),
+          ListTile(
+              title: Text('Favorite', textAlign: TextAlign.right),
+              trailing: Icon(Icons.favorite, size: 22),
+              onTap: () => Navigator.pop(context)),
+          ListTile(
+              title: Text('Settings', textAlign: TextAlign.right),
+              trailing: Icon(Icons.settings, size: 22),
+              onTap: () => Navigator.pop(context)),
+          ListTile(
+              title: Text('Logout', textAlign: TextAlign.right),
+              trailing: Icon(Icons.exit_to_app, size: 22),
+              onTap: () => Navigator.pop(context)),
+        ],
+      ),
     );
   }
 }
